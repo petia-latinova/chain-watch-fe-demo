e2076c31bfa79c61336ba3f79311cdb21aa1f6bc
Petia Latinova <petia.latinova@limechain.tech>
2025-12-12 13:16:14 +0200

    chore: update contract address and install wagmi and privy dependencies

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,7 +1,7 @@
 export const TOKEN_ADDRESSES = {
-  USDC: '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238', // Ethereum Sepolia USDC
-  EURC: '0x08210F9170F89Ab7658F0B5E3fF39b0E03C594D4', // Ethereum Sepolia EURC
-  // TODO: custom token address
+  'USDC': '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238', // Ethereum Sepolia USDC
+  'EURC': '0x08210F9170F89Ab7658F0B5E3fF39b0E03C594D4', // Ethereum Sepolia EURC
+  'CW-ERC20': '0xc2c9a6d4c2699349f69de33df8ed8a90db908944', // Ethereum Sepolia CW-ERC20:
 };
 
 export const AVAILABLE_SYMBOLS = Object.keys(TOKEN_ADDRESSES);
